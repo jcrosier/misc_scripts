4d327afa3ce8b744542cad6e5ffe4d92d12b25fc
Jonny Crosier <j.crosier@manchester.ac.uk>
2024-04-18 14:21:41 +0100

    Tephi tools now create and checks archives for data, and creates consistent tephis with static anchor

--- plot_tephi.py
+++ plot_tephi.py
@@ -1,142 +1,146 @@
+import os.path
 import matplotlib.pyplot as plt
 import tephi
-import time
-import copy
+import requests
+import pandas
 from datetime import datetime, timedelta
 from siphon.simplewebservice.wyoming import WyomingUpperAir
 
 
+NUMBER_TRIES = 20
 CONST_G = 9.81
 CONST_R = 287
 CONST_K = 273.15
 
 
-sites = {'names': ['CAMBORNE', 'BARROW', 'MIAMI', 'BECHAR', 'MCMURDO'],
-             'ids': ['03808', '70026', '72202', '60571', '89664']}
+class TimeInterval:
+    def __init__(self, start, end, delta):
+        self.start = start
+        self.end = end
+        self.delta = delta
 
-sites_2 = {'names': ['BARROW', 'MIAMI'],
-             'ids': ['70026', '72202']}
-
-dates = [datetime(2024, 3, 20, 00), datetime(2024, 3, 22, 00), timedelta(hours=12)]
-
-date = datetime(2024, 3, 26, 00)
 
 class Dataset:
-    def __init__(self, p_data, t_data, color, label, group):
-        self.data = zip(p_data, t_data)
-        self.color = color
-        self.label = label
-        self.group = group
+    def __init__(self, dataframe):
+        p_data = [p for p in dataframe['pressure'].tolist() if p > 100]
+        self.temperature = zip(p_data, dataframe['temperature'].tolist())
+        self.dewpoint = zip(p_data, dataframe['dewpoint'].tolist())
 
 
-#todo check if file/data already exists before attempting to download
-def download_sonde_to_csv(site_list, date_list, destination):
-    for idx, site in enumerate(site_list['ids']):
-        site_name = site_list['names'][idx]
-        current_date = date_list[0]
+class Isobar:
+    def __init__(self, pressure, t_start, t_end, t_delta):
+        num_values = 1 + int((t_end - t_start) / t_delta)
+        self.temperature = zip([pressure]*num_values, [*range(t_start, t_end, t_delta)])
+
+
+class Isotherm:
+    def __init__(self, temperature, p_start, p_end):
+        self.temperature = zip([p_start, p_end], [temperature]*2)
+
+
+class IsoEntropy:
+    def __init__(self, theta, p_start, p_end):
+        self.temperature = zip([p_start, p_end],
+                               [tephi.transforms.convert_pt2pT(press, theta)[1] for press in [p_start, p_end]])
+
+
+class StandardAtmosphere:
+    def __init__(self, pressure_data):
+        self.temperature = zip(pressure_data, [self.calc_standard_temp(pressure) for pressure in pressure_data])
+
+    @staticmethod
+    def calc_standard_temp(pressure):
+        if pressure > 226:
+            t0 = 288
+            p0 = 1000
+            lapse_rate = 0.0065
+            altitude = (t0/lapse_rate)*(1-((pressure/p0)**(CONST_R*lapse_rate/CONST_G)))
+            return (t0-CONST_K) - (altitude*lapse_rate)
+        else:
+            return -56.5
+
+
+def process_batch(site_dict, dates, data_path, plot_path, save_csv=True, save_plot=True, use_local=True):
+
+    for site_name in site_dict.keys():
+
+        current_date = dates.start
+        attempt = 0
+
         while True:
+            profile_name = site_name + '_' + current_date.strftime('%Y%m%d_%H')
+
+            # Check if the file already exists. If it does, skip the download process for this time interval
+            if os.path.exists(data_path + profile_name + '.csv') is True and use_local:
+                if save_plot:
+                    df = pandas.read_csv(data_path + profile_name + '.csv')
+                    plot_tephi(df, profile_name, plot_path)
+                current_date += dates.delta
+                continue
+
+            # Try to download data from the Wyoming archive
             try:
-                df = WyomingUpperAir.request_data(current_date, site)
+                attempt += 1
+                df = WyomingUpperAir.request_data(current_date, site_dict[site_name])
+
+            # No data found for this site+time combination
             except ValueError:
-                current_date += date_list[2]
-            except:# this is for busy server
-                pass
+                print(f'No data found for {profile_name}')
+                attempt = 0
+                current_date += dates.delta
+
+            # Server is busy
+            except requests.HTTPError:
+                if attempt > NUMBER_TRIES:
+                    print(f'Failed to download {profile_name}')
+                    attempt = 0
+                    current_date += dates.delta
+
+            # Download was successful -> save the dataframe to a csv
             else:
-                file_name = site_name + '_' + current_date.strftime('%Y%m%d_%H') + '.csv'
-                df.to_csv(destination + file_name)
-                current_date += date_list[2]
+                if save_csv:
+                    df.to_csv(data_path + profile_name + '.csv')
+                if save_plot:
+                    plot_tephi(df, profile_name, plot_path)
+                current_date += dates.delta
+                attempt = 0
+
+            # Check if there are any times left to download
             finally:
-                if current_date > date_list[1]:
+                if current_date > dates.end:
                     break
 
 
-
-def calc_standard_temp(p):
-    if p > 226:
-        t0 = 288
-        p0 = 1000
-        DALR= 0.0065
-        altitude = (t0/DALR)*(1-((p/p0)**(CONST_R*DALR/CONST_G)))
-        return (t0-CONST_K) - (altitude*DALR)
-    else:
-        return -56.5
-
-
-def plot_tephi(data_list, title, group_list):
+def plot_tephi(dataframe, name, path):
     tephi.MIXING_RATIO_LINE.update({'color': 'purple', 'linewidth': 0, 'linestyle': None})
     tephi.MIXING_RATIO_TEXT.update({"size": 0})
     tephi.WET_ADIABAT_LINE.update({'color': 'purple', 'linewidth': 0, 'linestyle': None})
     tephi.WET_ADIABAT_TEXT.update({"size": 0})
     tephi.ISOBAR_LINE.update({'color': 'gray'})
     tephi.ISOBAR_TEXT.update({'color': 'gray', 'size': 12})
-    tpg = tephi.Tephigram(isotherm_locator=tephi.Locator(20), dry_adiabat_locator=tephi.Locator(20))
-    for idx, data in enumerate(data_list):
-        if data.group in group_list:
-            tpg.plot(data.data, label=data.label, linewidth=3, color=data.color, linestyle=None, marker=None)
-        else:
-            tpg.plot(data.data, linewidth=0, color='white', linestyle=None, marker=None)
-    plt.title(title, fontsize=24)
+    tpg = tephi.Tephigram(isotherm_locator=tephi.Locator(20), dry_adiabat_locator=tephi.Locator(20),
+                          anchor=[(1000, -40), (50, -80)])
+    profile = Dataset(dataframe)
+    tpg.plot(profile.temperature, label='TEMPERATURE', linewidth=3, color='red', linestyle=None, marker=None)
+    tpg.plot(profile.dewpoint, label='DEWPOINT', linewidth=3, color='blue', linestyle=None, marker=None)
+    plt.title(name, fontsize=24)
     plt.yticks(fontsize=35)
-    plt.savefig(title+".jpg", dpi=150)
+    plt.savefig(path + name + ".jpg", dpi=150)
     plt.close('all')
 
-####################################################
-# Create a datetime object for the sounding and string of the station identifier.
+
+# Example of batch downloading and plotting
+sites = {
+    'CAMBORNE': '03808',
+    'BARROW': '70026',
+    'MIAMI': '72202'
+}
+interval = TimeInterval(datetime(2024, 3, 20, 00), datetime(2024, 3, 22, 00), timedelta(hours=12))
+process_batch(sites, interval, './data/', './plots/', save_csv=True, save_plot=True, use_local=True)
 
 
-def plot_it(data_list, sites, num, date):
-    df_camborne = data_list[0]
-    df_barrow = data_list[1]
-    df_miami = data_list[2]
-    df_bechar = data_list[3]
-    df_mcmurdo = data_list[4]
-
-    camborne_T = Dataset([p for p in df_camborne['pressure'].tolist() if p > 100], df_camborne['temperature'].tolist(), 'red', 'TEMPERATURE', 0)
-    camborne_Td = Dataset([p for p in df_camborne['pressure'].tolist() if p > 100], df_camborne['dewpoint'].tolist(), 'blue', 'DEWPOINT', 0)
-
-    barrow_T = Dataset([p for p in df_barrow['pressure'].tolist() if p > 100], df_barrow['temperature'].tolist(), 'red', 'TEMPERATURE', 1)
-    barrow_Td = Dataset([p for p in df_barrow['pressure'].tolist() if p > 100], df_barrow['dewpoint'].tolist(), 'blue', 'DEWPOINT', 1)
-
-    miami_T = Dataset([p for p in df_miami['pressure'].tolist() if p > 100], df_miami['temperature'].tolist(), 'red', 'TEMPERATURE', 2)
-    miami_Td = Dataset([p for p in df_miami['pressure'].tolist() if p > 100], df_miami['dewpoint'].tolist(), 'blue', 'DEWPOINT', 2)
-
-    bechar_T = Dataset([p for p in df_bechar['pressure'].tolist() if p > 100], df_bechar['temperature'].tolist(), 'red', 'TEMPERATURE', 3)
-    bechar_Td = Dataset([p for p in df_bechar['pressure'].tolist() if p > 100], df_bechar['dewpoint'].tolist(), 'blue', 'DEWPOINT', 3)
-
-    mcmurdo_T = Dataset([p for p in df_mcmurdo['pressure'].tolist() if p > 100], df_mcmurdo['temperature'].tolist(), 'red', 'TEMPERATURE', 4)
-    mcmurdo_Td = Dataset([p for p in df_mcmurdo['pressure'].tolist() if p > 100], df_mcmurdo['dewpoint'].tolist(), 'blue', 'DEWPOINT', 4)
-
-    plot_list = [camborne_Td, camborne_T, barrow_Td, barrow_T, miami_Td, miami_T, bechar_Td, bechar_T, mcmurdo_Td, mcmurdo_T]
-    time_string = date.strftime("%Y%m%d_%H")
-    plot_tephi(plot_list, sites['names'][num]+'_'+time_string, [num])
-
-def download_list(sites_dictionary, date):
-    data = []
-    pos = 0
-    num = len(sites_dictionary['ids'])
-    print('hello')
-    while True:
-        site_id = sites_dictionary['ids'][pos]
-        try:
-            new_list = copy.deepcopy(WyomingUpperAir.request_data(date, site_id))
-        except:
-            time.sleep(0.1)
-        else:
-            data.append(new_list)
-            pos += 1
-        if pos == num:
-            break
-
-    return data
-
-
-def plot_all(datasets, sites, date):
-    num = len(datasets)
-    for i in range(num):
-        plot_it(datasets, sites, i, date)
-
-
-#datasets = download_list(sites, date)
-#plot_all(datasets, sites, date)
-
-download_sonde_to_csv(sites_2, dates, './data/')
+# Example Isolines
+# SAM = StandardAtmosphere([*range(1000, 50, -50)])
+# isobar_850 = Isobar(850, -20, 50, 10)
+# isotherm_15 = Isotherm(15, 1000, 500)
+# entropy_20 = IsoEntropy(20, 1000, 500)
