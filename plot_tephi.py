ddcb79941e36a78eb8e035872d58bf635a46f082
Jonny Crosier <j.crosier@manchester.ac.uk>
2024-04-18 16:26:20 +0100

    Refactored to allow batch plotting. Also moved graphing options to global vars

--- plot_tephi.py
+++ plot_tephi.py
@@ -6,11 +6,26 @@ import pandas
 from datetime import datetime, timedelta
 from siphon.simplewebservice.wyoming import WyomingUpperAir
 
-
+# Number of attempts to download a file from the server
 NUMBER_TRIES = 20
+
+# Physical constants
 CONST_G = 9.81
 CONST_R = 287
 CONST_K = 273.15
+
+# Graphing options
+P_DATA_THRESHOLD = 100
+ISOBAR_INTERVAL = 100
+T_INCREMENT = 20
+ANCHOR = [(1000, -40), (50, -80)]   # [(P_bottom, T_bottom),(P_top, T_top)]
+SIMPLE_PLOT = True
+
+
+class Profile:
+    def __init__(self, dataframe):
+        self.T = TempDataframe(dataframe)
+        self.Td = DewDataframe(dataframe)
 
 
 class TimeInterval:
@@ -20,33 +35,57 @@ class TimeInterval:
         self.delta = delta
 
 
-class Dataset:
+class TempDataframe:
     def __init__(self, dataframe):
-        p_data = [p for p in dataframe['pressure'].tolist() if p > 100]
-        self.temperature = zip(p_data, dataframe['temperature'].tolist())
-        self.dewpoint = zip(p_data, dataframe['dewpoint'].tolist())
+        p_data = [p for p in dataframe['pressure'].tolist() if p > P_DATA_THRESHOLD]
+        self.data = zip(p_data, dataframe['temperature'].tolist())
+        self.color = 'red'
+        self.name = "TEMPERATURE"
+
+
+class DewDataframe:
+    def __init__(self, dataframe):
+        p_data = [p for p in dataframe['pressure'].tolist() if p > P_DATA_THRESHOLD]
+        self.data = zip(p_data, dataframe['dewpoint'].tolist())
+        self.color = 'blue'
+        self.name = "DEWPOINT"
+
+
+class Empty:
+    def __init__(self):
+        self.data = zip([850], [0])
+        self.color = 'white'
+        self.name = ""
 
 
 class Isobar:
     def __init__(self, pressure, t_start, t_end, t_delta):
         num_values = 1 + int((t_end - t_start) / t_delta)
-        self.temperature = zip([pressure]*num_values, [*range(t_start, t_end, t_delta)])
+        self.data = zip([pressure]*num_values, [*range(t_start, t_end, t_delta)])
+        self.color = 'green'
+        self.name = "ISOBAR " + str(pressure) + " mb"
 
 
 class Isotherm:
     def __init__(self, temperature, p_start, p_end):
-        self.temperature = zip([p_start, p_end], [temperature]*2)
+        self.data = zip([p_start, p_end], [temperature]*2)
+        self.color = 'purple'
+        self.name = "ISOTHERM " + str(temperature) + " degC"
 
 
 class IsoEntropy:
     def __init__(self, theta, p_start, p_end):
-        self.temperature = zip([p_start, p_end],
-                               [tephi.transforms.convert_pt2pT(press, theta)[1] for press in [p_start, p_end]])
+        self.data = zip([p_start, p_end],
+                        [tephi.transforms.convert_pt2pT(press, theta)[1] for press in [p_start, p_end]])
+        self.color = 'orange'
+        self.name = "ISENTROPE (constant THETA) " + str(theta) + " degC"
 
 
 class StandardAtmosphere:
     def __init__(self, pressure_data):
-        self.temperature = zip(pressure_data, [self.calc_standard_temp(pressure) for pressure in pressure_data])
+        self.data = zip(pressure_data, [self.calc_standard_temp(pressure) for pressure in pressure_data])
+        self.color = 'pink'
+        self.name = "1976 STANDARD ATMOSPHERE"
 
     @staticmethod
     def calc_standard_temp(pressure):
@@ -74,7 +113,8 @@ def process_batch(site_dict, dates, data_path, plot_path, save_csv=True, save_pl
             if os.path.exists(data_path + profile_name + '.csv') is True and use_local:
                 if save_plot:
                     df = pandas.read_csv(data_path + profile_name + '.csv')
-                    plot_tephi(df, profile_name, plot_path)
+                    data = Profile(df)
+                    plot_tephi([data.T, data.Td], profile_name, plot_path)
                 current_date += dates.delta
                 continue
 
@@ -82,6 +122,7 @@ def process_batch(site_dict, dates, data_path, plot_path, save_csv=True, save_pl
             try:
                 attempt += 1
                 df = WyomingUpperAir.request_data(current_date, site_dict[site_name])
+                data = Profile(df)
 
             # No data found for this site+time combination
             except ValueError:
@@ -101,7 +142,7 @@ def process_batch(site_dict, dates, data_path, plot_path, save_csv=True, save_pl
                 if save_csv:
                     df.to_csv(data_path + profile_name + '.csv')
                 if save_plot:
-                    plot_tephi(df, profile_name, plot_path)
+                    plot_tephi([data.T, data.Td], profile_name, plot_path)
                 current_date += dates.delta
                 attempt = 0
 
@@ -111,36 +152,46 @@ def process_batch(site_dict, dates, data_path, plot_path, save_csv=True, save_pl
                     break
 
 
-def plot_tephi(dataframe, name, path):
-    tephi.MIXING_RATIO_LINE.update({'color': 'purple', 'linewidth': 0, 'linestyle': None})
-    tephi.MIXING_RATIO_TEXT.update({"size": 0})
-    tephi.WET_ADIABAT_LINE.update({'color': 'purple', 'linewidth': 0, 'linestyle': None})
-    tephi.WET_ADIABAT_TEXT.update({"size": 0})
+def plot_tephi(data_list, name, path):
+
+    if SIMPLE_PLOT:
+        tephi.MIXING_RATIO_LINE.update({'color': 'purple', 'linewidth': 0, 'linestyle': None})
+        tephi.MIXING_RATIO_TEXT.update({"size": 0})
+        tephi.WET_ADIABAT_LINE.update({'color': 'purple', 'linewidth': 0, 'linestyle': None})
+        tephi.WET_ADIABAT_TEXT.update({"size": 0})
     tephi.ISOBAR_LINE.update({'color': 'gray'})
     tephi.ISOBAR_TEXT.update({'color': 'gray', 'size': 12})
-    tpg = tephi.Tephigram(isotherm_locator=tephi.Locator(20), dry_adiabat_locator=tephi.Locator(20),
-                          anchor=[(1000, -40), (50, -80)])
-    profile = Dataset(dataframe)
-    tpg.plot(profile.temperature, label='TEMPERATURE', linewidth=3, color='red', linestyle=None, marker=None)
-    tpg.plot(profile.dewpoint, label='DEWPOINT', linewidth=3, color='blue', linestyle=None, marker=None)
+    tephi.ISOBAR_SPEC = [(ISOBAR_INTERVAL, None)]
+
+    tpg = tephi.Tephigram(isotherm_locator=tephi.Locator(T_INCREMENT), dry_adiabat_locator=tephi.Locator(T_INCREMENT),
+                          anchor=ANCHOR)
+    for data in data_list:
+        tpg.plot(data.data, label=data.name, linewidth=3, color=data.color, linestyle=None, marker=None)
     plt.title(name, fontsize=24)
     plt.yticks(fontsize=35)
     plt.savefig(path + name + ".jpg", dpi=150)
     plt.close('all')
 
 
-# Example of batch downloading and plotting
+# Example of batch downloading (Wyoming database) and plotting
+# -------------------------------------------------------------
 sites = {
-    'CAMBORNE': '03808',
     'BARROW': '70026',
     'MIAMI': '72202'
 }
-interval = TimeInterval(datetime(2024, 3, 20, 00), datetime(2024, 3, 22, 00), timedelta(hours=12))
-process_batch(sites, interval, './data/', './plots/', save_csv=True, save_plot=True, use_local=True)
+time_interval = TimeInterval(datetime(2024, 3, 20, 00), datetime(2024, 3, 21, 00), timedelta(hours=12))
+process_batch(sites, time_interval, './data/', './plots/', save_csv=True, save_plot=True, use_local=True)
 
 
-# Example Isolines
-# SAM = StandardAtmosphere([*range(1000, 50, -50)])
-# isobar_850 = Isobar(850, -20, 50, 10)
-# isotherm_15 = Isotherm(15, 1000, 500)
-# entropy_20 = IsoEntropy(20, 1000, 500)
+# Example plots for Isolines and Standard Atmosphere Model
+# -------------------------------------------------------------
+SAM = StandardAtmosphere([*range(1000, 50, -10)])
+isobar_850 = Isobar(850, -40, 30, 10)
+isotherm_m20 = Isotherm(-20, 1000, 300)
+entropy_20 = IsoEntropy(20, 1000, 250)
+plot_tephi([SAM, isobar_850, isotherm_m20, entropy_20], 'ISOLINES', './plots/')
+
+# Example Blank Tephigram
+# -------------------------------------------------------------
+empty = Empty()
+plot_tephi([empty], 'EMPTY', './plots/')
